08b3566192b2709dd7c01bfea3611af0baed0a73
TJ Bai <tjianbai@gmail.com>
2024-12-11 14:48:23 -0500

    resolve conflicts

--- sample.py
+++ sample.py
@@ -19,6 +19,26 @@ def parse_response(tok_ids, tokenizer, last='<|end_header_id|>'):
     ind = resp.rindex(last)
     return resp[ind+len(last):]
 
+def load_batches(items, B):
+    for i in range(0, len(items), B):
+        yield items[i:i + B]
+
+def process(batch, pipe, tokenizer, pipe_kwargs):
+    messages = [{'role': 'user', 'content': format_prompt(item)} for item in batch]
+    gens = pipe(messages, **pipe_kwargs)
+
+    results = []
+    for i, item in enumerate(batch):
+        s = i * pipe_kwargs['num_return_sequences']
+        e = s + pipe_kwargs['num_return_sequences']
+        item_gens = gens[s:e]
+
+        tok_ids = [x['generated_token_ids'] for x in item_gens]
+        resps = [parse_response(resp, tokenizer) for resp in tok_ids]
+        results.append({'item': item, 'resps': resps, 'num_toks': list(map(len, tok_ids))})
+
+    return results
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config')
@@ -46,13 +66,11 @@ def main():
     }
 
     with open(config['output'], 'w') as f:
-        for item in tqdm(dataset[config['split']]):
-            messages = [{'role': 'user', 'content': format_prompt(item)}]
-            gen = pipe(messages, **pipe_kwargs)
-            tok_ids = [x['generated_token_ids'] for x in gen]
-            resps = [parse_response(resp, tokenizer) for resp in tok_ids]
-            json.dump({'item': item, 'resps': resps, 'num_toks': list(map(len, tok_ids))}, f)
-            f.write('\n')
+        for batch in tqdm(load_batches(dataset[config['split']], config['B'])):
+            results = process(batch, pipe, tokenizer, pipe_kwargs)
+            for result in results:
+                json.dump(result, f)
+                f.write('\n')
 
 if __name__ == '__main__':
     main()

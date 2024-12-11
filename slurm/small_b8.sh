8727ef6b139caec254074b2c8037f984ca777436
tjbai <tjianbai@gmail.com>
2024-12-11 15:03:38 -0500

    larger bsz

---
+++ slurm/small_b8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=small_b8
+#SBATCH -A jeisner1_gpu
+#SBATCH --partition=a100
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --mem=80G
+#SBATCH --time=8:00:0
+#SBATCH --output=small_b8.out
+
+ml anaconda
+conda activate llmr
+python3 sample.py --config=configs/small_b8.yml

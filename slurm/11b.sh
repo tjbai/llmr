b79d53e3103c3e6cf2488310d5dc0f0420838edb
tjbai <tjianbai@gmail.com>
2024-12-12 17:37:57 -0500

    sanity check results

--- slurm/11b.sh
+++ slurm/11b.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=small
+#SBATCH --job-name=11b
 #SBATCH -A jeisner1_gpu
 #SBATCH --partition=a100
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --mem=80G
 #SBATCH --time=8:00:0
-#SBATCH --output=small.out
+#SBATCH --output=11b.out
 
 ml anaconda
 conda activate llmr
-python3 sample.py --config=configs/small.yml
+python3 sample.py --config=configs/11b.yml

56d341ad5d54de1e56e0eac53915172ee46f5f39
tjbai <tjianbai@gmail.com>
2024-12-12 19:42:54 -0500

    1b and 8b outputs

--- slurm/8b.sh
+++ slurm/8b.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=11b
+#SBATCH --job-name=8b
 #SBATCH -A jeisner1_gpu
 #SBATCH --partition=a100
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --mem=80G
 #SBATCH --time=8:00:0
-#SBATCH --output=11b.out
+#SBATCH --output=8b.out
 
 ml anaconda
 conda activate llmr
-python3 sample.py --config=configs/11b.yml
+python3 sample.py --config=configs/8b.yml

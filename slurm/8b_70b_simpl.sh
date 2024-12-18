79604819e0c3848c2c27114e806a34d08b590805
tjbai <tjianbai@gmail.com>
2024-12-18 13:22:20 -0500

    simpl checkpoints

---
+++ slurm/8b_70b_simpl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=8b_70b_simpl
+#SBATCH -A jeisner1_gpu
+#SBATCH --partition=a100
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --mem=80G
+#SBATCH --time=8:00:0
+#SBATCH --output=8b_70b_simpl.out
+
+ml anaconda
+conda activate llmr
+python3 router.py --config=configs/router/8b_70b_simpl.yml --train
